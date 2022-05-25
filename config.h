9c48200931bd63520be6b1f57b35b18ede0c7963
rafasaurus <rafaelkgrigoryan@gmail.com>
2022-05-25 10:37:02 +0400

    Update dwmlbocks

--- config.h
+++ config.h
@@ -5,32 +5,33 @@ static const Block blocks[] = {
 	// {"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	// {"",	"sb-tasks",	10,	26},
 	// {"",	"sb-music",	0,	11},
-	{"",	"sb-pacpackages",	200,	8},
-	{"",	"sb-todo",	200,	11},
-	{"",	"sb-news",		0,	6},
 	/* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000,	22}, */
 	/* {"",	"sb-price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
 	/* {"",	"sb-price link \"Chainlink\" 🔗",			300,	25}, */
 	/* {"",	"sb-price xmr \"Monero\" 🔒",			9000,	24}, */
 	/* {"",	"sb-price eth Ethereum 🍸",	9000,	23}, */
-	/* {"",	"sb-price btc Bitcoin 💰",				9000,	21}, */
+	/* {"",	"sb-moonphase",	        18000,	17}, */
+	// {"",	"sb-forecast",	        18000,	5},
+	// {"",	"sb-mailbox",	        180,	12},
+	// {"",	"sb-nettraf",	        1,	16},
 	// {"",	"sb-torrent",	20,	7},
-	{"",	"sb-memory",	10,	14},
-	{"",	"sb-disk",	    1,	5},
-	{"",	"sb-cpu",		2,	18},
-	/* {"",	"sb-moonphase",	18000,	17}, */
-	// {"",	"sb-forecast",	18000,	5},
-	// {"",	"sb-mailbox",	180,	12},
-	// {"",	"sb-nettraf",	1,	16},
-	{"",	"sb-battery",	5,	3},
-	{"",	"sb-volume",	0,	10},
-	{"",	"sb-clock",	    60,	1},
-	{"",	"sb-internet",	5,	4},
-	// {"",	"sb-help-icon",	0,	15},
+	// {"",	"sb-help-icon",	        0,	15},
+	{"",	"sb-pacpackages",	    24400,	8},
+	{"",	"sb-todo",	            200,	11},
+	{"",	"sb-news",		        0,	6},
+	{"",	"sb-price btc Bitcoin 💰",				9000,	21},
+	{"",	"sb-memory",	        10,	14},
+	{"",	"sb-disk",	            60,	5},
+	{"",	"sb-cpu",		        3,	18},
+	{"",	"sb-battery",	        5,	3},
+	{"",	"sb-volume",	        0,	10},
+	{"",	"sb-clock",	            60,	1},
+	{"",	"sb-internet",	        5,	4},
+	{"",	"sb-menu",	            0,	9},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = "|";
+static char *delim = " ";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
